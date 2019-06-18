db1b22d4b58ff1cba9a7358c6785bc2fc187b4fe
Julián <judramirezva@gmail.com>
2019-06-17 19:32:47 -0500

    Reto Suma Números

---
+++ index.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/' do
+	@count = params[:item]
+	if @count == nil
+		@count = 0
+	end
+  erb :index
+end
+
+get '/contador' do
+	@count = params[:c].to_i + 1
+	redirect "/?item=#{@count}"
+end
